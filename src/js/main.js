972c4ce1e4537a0b0fa508490344a502d29b85e6
Maciej Goral <maciej636107@gmail.com>
2022-05-27 17:32:39 +0200

    Table styling and loading

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ const btn = document.querySelector(".btnOpenPopup");
 const span = document.querySelector(".close");
 const popupText = document.querySelector("#popupText");
 const resetButton = document.querySelector(".btnReset");
+
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
@@ -23,15 +24,23 @@ resetButton.onclick = function() {
     popupText.innerHTML="You have clicked <b>"+localStorage.getItem("numberOfClicks")+" times</b> to related button.";
     resetButton.style.display="none";
 }
+
+function hideTable(){
+  const table = document.querySelector("#table");
+  table.style.display='none'; 
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  hideTable();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    hideTable();
   }
 }
 
@@ -41,23 +50,36 @@ async function getData(){
   return data
 }
 
+//delay to show loading, unnessesary in the real application
+function delay(time) {                                  
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
 async function addDataToTable()
 {
+  document.querySelector("#loading").style.display="block";
+  await delay(3000);
   await getData().then(data => {
+    document.querySelector("#loading").style.display="none";
     const table = document.querySelector("#table");
-    data.forEach(element => {
-      var row = table.insertRow(-1);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      var cell4 = row.insertCell(3);
-      var cell5 = row.insertCell(4);
-      cell1.innerHTML = element.name;
-      cell2.innerHTML = element.email;
-      cell3.innerHTML = element.address.city+" "+element.address.street+" "+element.address.suite;
-      cell4.innerHTML = element.phone;
-      cell5.innerHTML = element.company.name;
-    });
+    table.style.display='inline-block'; 
+    //only add data if it was not already added
+    if (table.rows.length == 1)
+    {
+      data.forEach(element => {
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        cell1.innerHTML = element.name;
+        cell2.innerHTML = element.email;
+        cell3.innerHTML = element.address.city+" "+element.address.street+" "+element.address.suite;
+        cell4.innerHTML = element.phone;
+        cell5.innerHTML = element.company.name;
+      });
+    }
   });
 }
 
