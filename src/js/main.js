cc5e36dcde7ec67a409e8f9c9f36f76b540cf4d6
Maciej Goral <maciej636107@gmail.com>
2022-05-27 15:14:26 +0200

    Proper styling and responsiveness

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,23 @@
 const modal = document.querySelector(".modal");
-const btn = document.querySelector(".openPopupButton");
+const btn = document.querySelector(".btnOpenPopup");
 const span = document.querySelector(".close");
-const popupText = document.querySelector(".popupText");
-const resetButton = document.querySelector(".resetButton");
+const popupText = document.querySelector("#popupText");
+const resetButton = document.querySelector(".btnReset");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
-  var numberofClicks=localStorage.getItem("numberOfClicks");
-  if(numberofClicks == null)
-  {
-    localStorage.setItem("numberOfClicks", 1);
-  }
-  else
-  {
-    numberofClicks++;
-  }
-  localStorage.setItem("numberOfClicks", numberofClicks);
+  var numberOfClicks=localStorage.getItem("numberOfClicks");
+  //set number of clicks
+  numberOfClicks == null ? numberOfClicks=1 : numberOfClicks++;
+  localStorage.setItem("numberOfClicks", numberOfClicks);
+  //display number of clicks
   popupText.innerHTML="You have clicked <b>"+localStorage.getItem("numberOfClicks")+" times</b> to related button.";
-  if(numberofClicks < 5)
-  {
-    resetButton.style.display="none";
-  }
-  else
-  {
-    resetButton.style.display="block";
-  }
+  //display or hide reset button
+  numberOfClicks < 5?resetButton.style.display="none":resetButton.style.display="block"; 
 }
 
+// When the user clicks on the reset button set number of clicks to 0
 resetButton.onclick = function() {
     localStorage.setItem("numberOfClicks", 0);
     popupText.innerHTML="You have clicked <b>"+localStorage.getItem("numberOfClicks")+" times</b> to related button.";
