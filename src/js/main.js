49e1bc0c8c8ce6f037de5584b5ec320e87e74a2f
Maciej Goral <maciej636107@gmail.com>
2022-05-27 16:37:08 +0200

    Fetching data to table

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ const btn = document.querySelector(".btnOpenPopup");
 const span = document.querySelector(".close");
 const popupText = document.querySelector("#popupText");
 const resetButton = document.querySelector(".btnReset");
-
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
@@ -15,6 +14,7 @@ btn.onclick = function() {
   popupText.innerHTML="You have clicked <b>"+localStorage.getItem("numberOfClicks")+" times</b> to related button.";
   //display or hide reset button
   numberOfClicks < 5?resetButton.style.display="none":resetButton.style.display="block"; 
+  addDataToTable();
 }
 
 // When the user clicks on the reset button set number of clicks to 0
@@ -34,3 +34,30 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+async function getData(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  var data = await response.json();
+  return data
+}
+
+async function addDataToTable()
+{
+  await getData().then(data => {
+    const table = document.querySelector("#table");
+    data.forEach(element => {
+      var row = table.insertRow(-1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      cell1.innerHTML = element.name;
+      cell2.innerHTML = element.email;
+      cell3.innerHTML = element.address.city+" "+element.address.street+" "+element.address.suite;
+      cell4.innerHTML = element.phone;
+      cell5.innerHTML = element.company.name;
+    });
+  });
+}
+
